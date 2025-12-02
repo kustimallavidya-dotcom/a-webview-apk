43e71d3ac1f184123a8e53cd2b51812fab95da74
kustimallavidya-dotcom <kustimallavidya@gmail.com>
2025-12-02 19:43:34 +0530

    Refactor MainActivity to use a class-level WebView

--- app/src/main/java/MainActivity.kt
+++ app/src/main/java/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.my.pwa.webview
+package com.mypwa.webview
 
 import android.annotation.SuppressLint
 import android.os.Bundle
@@ -8,16 +8,33 @@ import androidx.activity.ComponentActivity
 
 class MainActivity : ComponentActivity() {
 
+    private lateinit var webView: WebView
+
     @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val webView = WebView(this)
+        webView = WebView(this)
+
+        // Web settings
         webView.settings.javaScriptEnabled = true
         webView.settings.domStorageEnabled = true
+
+        // Open links inside the app
         webView.webViewClient = WebViewClient()
+
+        // इथे तुझी Netlify PWA URL
         webView.loadUrl("https://milind-singstar.netlify.app")
 
         setContentView(webView)
+    }
+
+    override fun onBackPressed() {
+        // webview मध्ये मागे जाण्यासाठी
+        if (this::webView.isInitialized && webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
     }
 }
