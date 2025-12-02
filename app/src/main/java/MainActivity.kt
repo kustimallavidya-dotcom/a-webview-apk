4c7a0fe0be7dd70e08ae7cfef07c58059262ae7a
kustimallavidya-dotcom <kustimallavidya@gmail.com>
2025-12-02 19:25:10 +0530

    Implement WebView in MainActivity
    Refactored MainActivity to use WebView for loading a URL.

--- app/src/main/java/MainActivity.kt
+++ app/src/main/java/MainActivity.kt
@@ -1,20 +1,23 @@
-<?xml version="1.0" encoding="utf-8"?>
-<manifest xmlns:android="http://schemas.android.com/apk/res/android"
-    package="com.my.pwa.webview">
+package com.my.pwa.webview
 
-    <uses-permission android:name="android.permission.INTERNET" />
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.activity.ComponentActivity
 
-    <application
-        android:allowBackup="true"
-        android:label="My Web App"
-        android:theme="@style/Theme.AppCompat.Light.NoActionBar">
-        <activity android:name=".MainActivity"
-            android:exported="true">
-            <intent-filter>
-                <action android:name="android.intent.action.MAIN"/>
-                <category android:name="android.intent.category.LAUNCHER"/>
-            </intent-filter>
-        </activity>
-    </application>
+class MainActivity : ComponentActivity() {
 
-</manifest>
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val webView = WebView(this)
+        webView.settings.javaScriptEnabled = true
+        webView.settings.domStorageEnabled = true
+        webView.webViewClient = WebViewClient()
+        webView.loadUrl("https://milind-singstar.netlify.app")
+
+        setContentView(webView)
+    }
+}
